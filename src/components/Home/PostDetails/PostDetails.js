2c9f03fd98b28b67b4cba69d9dfdd78a3fb7b57a
Jishnu Sardar <jishnu.sardar@bjitgroup.com>
2021-03-08 00:20:10 +0600

    first commit

---
+++ src/components/Home/PostDetails/PostDetails.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const PostDetails = () => {
+    const {id} = useParams();
+    const [post,setPost] = useState({});
+
+    useEffect(() =>{
+        const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+        fetch(url)
+        .then(res =>res.json())
+        .then(data =>setPost(data));
+    },[]);
+
+    
+    return (
+        <div>
+            <h1>Title: {post.title}</h1>
+            <p>Body: {post.body}</p>
+        </div>
+    );
+};
+
+export default PostDetails;
